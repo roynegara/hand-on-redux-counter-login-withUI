76044061e36ed985187d97b8d8fc36448831608f
roynegara <roybayunegara92@gmail.com>
2024-03-18 10:02:23 +0700

    repair header before is contition false did not disapear, and now disapear using config {isLoggedIn}

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ import Auth from "./components/Auth";
 import UserProfile from "./components/UserProfile";
 
 const App = () => {
-  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const { isLoggedIn } = useSelector((state) => state.isLoggedIn);
 
   console.log("App", isLoggedIn);
-
 
   return (
     <Fragment>
       <Header />
       {!isLoggedIn && <Auth />}
-
       {isLoggedIn && <UserProfile />}
       <Counter />
     </Fragment>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
-import {useSelector} from 'react-redux'
-import classes from './Header.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import {logout} from "../features/authSlice";
+import classes from "./Header.module.css";
 
 const Header = () => {
-  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const { isLoggedIn } = useSelector((state) => state.isLoggedIn);
+  const dispatch = useDispatch();
+ 
 
-  console.log('header', isLoggedIn)
+  console.log("header", isLoggedIn);
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  }
+ 
+  console.log('logoutheader', {logoutHandler})
 
   return (
     <header className={classes.header}>
       <h1>Dibimbing Redux Auth</h1>
-      {isLoggedIn && (
+      {isLoggedIn &&  (
         <nav>
           <ul>
             <li>
@@ -19,16 +28,16 @@ const Header = () => {
               <a href="/">My Sales</a>
             </li>
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler} >Logout</button>
             </li>
           </ul>
         </nav>
       )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 // import classes from './Header.module.css'
 
